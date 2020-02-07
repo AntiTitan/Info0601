3e2868c2059460f813d61cc3fcfb9b7c57d7cc5f
warn <animtenraw>
2020-02-07 18:22:27 +0100

    mina

--- TP6/client.c
+++ TP6/client.c
@@ -4,3 +4,15 @@ Envoie:
     TYPE_ENREGISTRER : une var de type enregistre_t
     TYPE_CLIENT : une var msgClient_t
 */
+
+#define MAX_CLIENT 10
+#define _XOPEN_SOURCE
+
+#include <string.h>
+#include <stdlib.h>     /* Pour exit, EXIT_FAILURE, EXIT_SUCCESS */
+#include <stdio.h>      /* Pour printf, perror */
+#include <sys/msg.h>    /* Pour msgget, msgsnd, msgrcv */
+#include <errno.h>      /* Pour errno */
+#include <sys/stat.h>   /* Pour S_IRUSR, S_IWUSR */
+
+#include "struct.h"
