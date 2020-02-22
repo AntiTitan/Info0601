66a65c76fda4a52c26b84ca00b88874c0f710198
Antinea <antineapintaux@gmail.com>
2020-02-22 21:21:20 +0100

    update

--- TP8/client.c
+++ TP8/client.c
@@ -5,9 +5,19 @@
 #include <sys/stat.h>   /* Pour S_IRUSR, S_IWUSR */
 #include "TP8.h"
 
+/**
+ * Génère un entier aléatoire dans un intervalle [a, b].
+ * @param a la borne minimale
+ * @param b la borne maximale
+ * @return l'entier aléatoire
+ */
+int alea(int a, int b) {
+  return rand() % (b - a + 1) + a;
+}
+
 
 int main(int argc, char* argv[]) {
-    int shmid, semid, i, CLE_S, CLE_M, c, l;
+    int shmid, semid, i, CLE_S, CLE_M, c, l, color;
     grille_t * grille;
 
     if (argc != 3) {
@@ -41,11 +51,99 @@ int main(int argc, char* argv[]) {
         exit(EXIT_FAILURE);
     }
 /*A mettre dans une boucle*/
-    /* Modification de la grille du segment de mémoire partagée */
-    srand(time(NULL)*getpid());
-    c = rand()%NB_C-1;
-    l = rand()%NB_L-1;
-    grille[c][l] = 'x';
+
+    while(1) {
+        /* Modification de la grille du segment de mémoire partagée */
+        srand(time(NULL)*getpid());
+        c = alea(0, NB_C-1);
+        l = alea(0, NB_L-1);
+        if (c==0 && l==0) {               /*coin haut gauche*/
+            Peux(NB_C*l+c, CLE_S);        /*case*/
+            Peux(NB_C*l+c+1, CLE_S);      /*droite*/
+            Peux(NB_C*(l+1)+c, CLE_S);    /*bas*/
+            color = grille.grille[c][l];
+            if (grille.grille[c][l+1] > color) color = grille.grille[c][l+1];
+            if (grille.grille[c+1][l] > color) color = grille.grille[c+1][l];
+            color++;
+            grille.grille[c][l] = color % 5;
+            grille.grille[c+1][l] = color % 5;
+            grille.grille[c][l+1] = color % 5;
+            Vas(NB_C*l+c, CLE_S);
+            Vas(NB_C*l+c+1, CLE_S);
+            Vas(NB_C*(l+1)+c, CLE_S);
+        }
+        else if (c==0 && l==NB_L-1) {         /*coin bas gauche*/
+            Peux(NB_C*l+c, CLE_S);            /*case*/
+            Peux(NB_C*(l-1)+c, CLE_S);        /*haut*/
+            Peux(NB_C*l+c+1, CLE_S);          /*droite*/
+            color = grille.grille[c][l];
+            if (grille.grille[c][l-1] > color) color = grille.grille[c][l-1];
+            if (grille.grille[c+1][l] > color) color = grille.grille[c+1][l];
+            color++;
+            grille.grille[c][l] = color % 5;
+            grille.grille[c][l-1] = color % 5;
+            grille.grille[c+1][l] = color % 5;
+            Vas(NB_C*l, CLE_S);
+            Vas(NB_C*(l-1), CLE_S);
+            Vas(NB_C*l+1, CLE_S);
+        }
+        else if (c==NB_C-1 && l==NB_L-1) {/*coin bas droite*/
+            Peux(NB_C*l+c, CLE_S);
+            Peux(NB_C*(l-1)+c, CLE_S);
+            Peux(NB_C*l+c-1, CLE_S);
+            color = grille.grille[c][l];
+            if (grille.grille[c][l-1] > color) color = grille.grille[c][l-1];
+            if (grille.grille[c-1][l] > color) color = grille.grille[c-1][l];
+            color++;
+            grille.grille[c][l] = color % 5;
+            grille.grille[c-1][l] = color % 5;
+            grille.grille[c][l-1] = color % 5;
+            Vas(NB_C*l+c, CLE_S);
+            Vas(NB_C*(l-1)+c, CLE_S);
+            Vas(NB_C*l+c-1, CLE_S);
+        }
+        else if (c==NB_C-1 && l==0) {/*coin haut droite*/
+            Peux(NB_C*l+c, CLE_S);
+            Peux(NB_C*l+c-1, CLE_S);
+            Peux(NB_C*(l+1)+c, CLE_S);
+            color = grille.grille[c][l];
+            if (grille.grille[c][l+1] > color) color = grille.grille[c][l+1];
+            if (grille.grille[c-1][l] > color) color = grille.grille[c-1][l];
+            color++;
+            grille.grille[c][l] = color % 5;
+            grille.grille[c-1][l] = color % 5;
+            grille.grille[c][l+1] = color % 5;
+            Vas(NB_C*l+c, CLE_S);
+            Vas(NB_C*l+c-1, CLE_S);
+            Vas(NB_C*(l+1)+c, CLE_S);
+        }
+        else {
+            Peux(NB_C*l+c, CLE_S);
+            Peux(NB_C*l+c-1, CLE_S);/*gauche*/
+            Peux(NB_C*l+c+1, CLE_S);/*droite*/
+            Peux(NB_C*(l-1)+c, CLE_S);/*haut*/
+            Peux(NB_C*(l+1)+c, CLE_S);/*bas*/
+            color = grille.grille[c][l];
+            if (grille.grille[c-1][l] > color) color = grille.grille[c-1][l];
+            if (grille.grille[c+1][l] > color) color = grille.grille[c+1][l];
+            if (grille.grille[c][l-1] > color) color = grille.grille[c][l-1];
+            if (grille.grille[c][l+1] > color) color = grille.grille[c][l+1];
+            color++;
+            grille.grille[c][l] = color % 5;
+            grille.grille[c-1][l] = color % 5;
+            grille.grille[c+1][l] = color % 5;
+            grille.grille[c][l-1] = color % 5;
+            grille.grille[c][l+1] = color % 5;
+            Vas(NB_C*l+c, CLE_S);
+            Vas(NB_C*l+c-1, CLE_S);
+            Vas(NB_C*l+c+1, CLE_S);
+            Vas(NB_C*(l-1)+c, CLE_S);
+            Vas(NB_C*(l+1)+c, CLE_S);
+
+        }
+    }
+
+
 
     /* Détachement du segment de mémoire partagée */
     if(shmdt(grille) == -1) {
