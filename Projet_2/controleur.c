f1f449a5b8bbaf60d8423a9296b9b2649f81e508
Antinea <antineapintaux@gmail.com>
2020-02-25 19:10:25 +0100

    update projet

--- Projet_2/controleur.c
+++ Projet_2/controleur.c
@@ -1,6 +1,10 @@
 #include "f_controleur.h"
 
 int main (int argc, char * argv []){
+    int nbV, CLE_F, CLE_M, CLE_S;
+    int msqid, shmid, semid, size;
+    unsigned short val[NBSEM];/*tableau d'initialisation des sem*/
+    grille_t carte;
 
 /*Verification des arguments
     nom fichier carte
@@ -10,15 +14,48 @@ int main (int argc, char * argv []){
     cle tableau sémaphores
 */
 
+    if (argc != 6) {
+        fprintf(stderr, "Nombre d'arguments incorrect:  ./controleur ficCartte nbMaxVoiture CLE_FIL CLE_MEM, CLE_SEM\n");
+        exit(EXIT_FAILURE);
+    }
+    nbV = atoi(argv[2]);
+    CLE_F = atoi(argv[3]);
+    CLE_M = atoi(argv[4]);
+    CLE_S = atoi(argv[5]);
+    size = sizeof(carte);
+
+/*Allocation de la structure partagee*/
+
+    if ((carte = malloc(sizeof(grille_t)))==NULL) {
+        fprintf(stderr, "erreur malloc");
+        exit(EXIT_FAILURE);
+    }
+
+
 /*Création outils IPC*/
+
+    msqid = creerFile(CLE_F);
+    shmid = creerMemoire(CLE_M, size);
+    semid = creerSemaphores(CLE_S);
 
 /*Init outils IPC*/
 
-        /*Premier affichage simulation (on gère ça à la fin)*/
+    /* Initialisation du tab d'initialisation des sem*/
+    for (i=0; i<NBSEM; i++) {
+        val[i] = 1;
+    }
+
+    /* Initialisation des sémaphores */
+    if(semctl(semid, 0, SETALL, val) == -1) {
+        fprintf(stderr, "Erreur lors de l'initialisation des sémaphores ");
+        exit(EXIT_FAILURE);
+    }
+
+    /*Premier affichage simulation (on gère ça à la fin)*/
 /*Mise en attente messages sur file*/
 
 /*Mise à jour des infos suite à la reception d'un message*/
-        /*Mise à jour affichage (on gère ça à la fin) */
+    /*Mise à jour affichage (on gère ça à la fin) */
 /*Reception message : gérér
     - cas où une voiture change sa position (TYPE 3)
         -> modif info sur seg mem partagé
@@ -30,6 +67,10 @@ int main (int argc, char * argv []){
 /*Envoi SIGINT aux programmes voiture*/
 
 /*suppression outils IPC */
+
+    supprimerFile(msqid);
+    supprimerMemoire(shmid);
+    supprimerSemaphores(semid);
 
 /*arrêt prgm*/
 
--- Projet_2/f_controleur.h
+++ Projet_2/f_controleur.h
@@ -1,6 +1,11 @@
 #ifndef F_CONTROLEUR_H
 #define F_CONTROLEUR_H
 
+#include <sys/msg.h>    /* Pour msgget, msgctl */
+#include <sys/sem.h>    /* Pour semget, semctl */
+#include <sys/shm.h>    /* Pour shmget, shmctl */
+#include <errno.h>      /* Pour errno */
+
 #include "ncurses.h"
 #include "struct.h"
 
@@ -8,6 +13,20 @@
 #define TYPE_ENVOI_CONFIG 2
 #define TYPE_MODIF_CARTE  3
 
+#define NB_C    30      /* Nombre de colonnes matrice*/
+#define NB_L    15      /* Nombre de lignes matrice*/
+#define NBSEM    NB_C*NB_L
 
+typedef struct {
+    unsigned char grille[NB_L][NB_C];
+} grille_t;
+
+void creerFile(int msqid);
+void creerMemoire(int shmid);
+void creerSemaphores(int semid);
+
+void supprimerFile(int msqid);
+void supprimerMemoire(int shmid);
+void supprimerSemaphores(int semid);
 
 #endif
